bbf3e9d20b23f7fb395f923d273a119498649fcc
Michael Leroy <mickjleroy@gmail.com>
2016-08-06 19:06:23 +1000

    - creating META-INF folder - creating jcr_root contents - changed structure of fixtures

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ var through = require('through2');
 var assign  = require('object-assign');
 var chalk   = require('chalk');
 var plur    = require('plur');
-var fs      = require('fs');
+var fs      = require('fs-extra');
+var tmp     = require('tmp');
+var _       = require('underscore');
 
 // grab the location from where the script is running from
-// i.e. ~/projects/myproj/node_modules/grunt-clientlibify/tasks/
+// i.e. ~/projects/myproj/node_modules/gulp-clientlibify/
 var taskDir     = __dirname;
 var PLUGIN_NAME = "gulp-clientlibify";
+
+var templates = {
+	designContent:    taskDir + '/templates/designContent.xml',
+	clientlibContent: taskDir + '/templates/clientlibContent.xml',
+	filterXml:        taskDir + '/templates/filter.xml',
+	folderContent:    taskDir + '/templates/folderContent.xml',
+	jcrRootContent:   taskDir + '/templates/jcrRootContent.xml',
+	propertiesXml:    taskDir + '/templates/properties.xml'
+}
 
 module.exports = function (options) {
 
@@ -36,6 +47,8 @@ module.exports = function (options) {
     // set the main category as the first categories entry
     options.category = options.categories[0];
 
+    options.isoDateTime = new Date().toISOString();
+
 	// validate mandatory config
     if(!options.cssDir && !options.jsDir) {
 		throw new gutil.PluginError(PLUGIN_NAME, '`cssDir` and/or `jsDir` must be provided');
@@ -54,6 +67,7 @@ module.exports = function (options) {
 	var jsFiles = [];
 	var cssPath = fs.realpathSync(options.cssDir);
 	var jsPath = fs.realpathSync(options.jsDir);
+	var fileUploaded = false;
 
 	function bufferContents(file, encoding, callback) {
 		if (file.isNull()) {
@@ -67,27 +81,73 @@ module.exports = function (options) {
 			return;
 		}
 
-		//TODO: add file to array
-		//TODO: add file entry to txt file
-
-		if(cssPath && path.extname(file.relative) == '.css' && isFileInPath(file, cssPath)) {
+		if(cssPath && (path.extname(file.relative) == '.css' || path.extname(file.relative) == '.less') && isFileInPath(file, cssPath)) {
 			cssFiles.push(file);
-			gutil.log(PLUGIN_NAME, chalk.green('✔ ') + file.relative);
 			fileCount++;
-		} else {
-			gutil.log(PLUGIN_NAME, chalk.red('x ') + file.relative);
 		}
 
-		
+		if(jsPath && path.extname(file.relative) == '.js' && isFileInPath(file, jsPath)) {
+			jsFiles.push(file);
+			fileCount++;
+		}
+
 		callback(null, file);
 	}
 
 	function endStream(callback) {
-		//TODO: build package
-		//TODO: upload package
-		gutil.log(cssFiles, ' CSS files added');
-		fileUploaded = true;
+	    // create a system tmp directory to store our tmp files`
+	    var tmpWorkingDir = tmp.dirSync({prefix: 'gulp_clientlibify_', unsafeCleanup: true}).name;
+    	gutil.log('Tmp directory is located at: ', tmpWorkingDir);
 
+		var jcrRootPath = path.join(tmpWorkingDir, '/jcr_root');
+	    var metaInfPath = path.join(tmpWorkingDir, '/META-INF');
+
+	    var clientlibRootDir        = path.join(jcrRootPath, '/etc/designs/', options.category);
+	    var clientlibFolderLocation = path.join(clientlibRootDir, '/clientlibs');
+	    var vaultPath               = path.join(metaInfPath, '/vault');
+
+    	fs.mkdirsSync(clientlibRootDir);
+
+	    // create .content.xml for `/jcr_root/` folder
+	    fs.copySync(templates.jcrRootContent, path.join(jcrRootPath, '/.content.xml'));
+
+	    // create .content.xml for `/jcr_root/etc/` folder
+	    fs.copySync(templates.folderContent, path.join(jcrRootPath, '/etc/.content.xml'));
+
+	    // create .content.xml for `/jcr_root/etc/designs` folder
+	    fs.copySync(templates.folderContent, path.join(jcrRootPath, '/etc/designs/.content.xml'));
+
+		// create .content.xml for `/jcr_root/etc/designs/<category>/` folder
+    	fs.copySync(templates.designContent, path.join(clientlibRootDir, '/.content.xml'));
+
+		// create .content.xml for `/jcr_root/etc/designs/<category>/clientlibs` folder
+	    var clientLibFileContents = _.template(fs.readFileSync(templates.clientlibContent, "utf8"))(options);
+	    fs.outputFileSync(path.join(clientlibFolderLocation,'/.content.xml'), clientLibFileContents);
+
+	    // create META-INF folder
+	    fs.mkdirsSync(vaultPath);
+
+	    // create `META-INF/vault/filter.xml` file
+	    var filterFileContents = _.template(fs.readFileSync(templates.filterXml, "utf-8"))(options);
+	    fs.outputFileSync(path.join(vaultPath, '/filter.xml'), filterFileContents);
+
+	    // create `META-INF/vault/properties.xml` file
+	    var propsFileContents = _.template(fs.readFileSync(templates.propertiesXml, "utf-8"))(options);
+	    fs.outputFileSync(path.join(vaultPath, '/properties.xml'), propsFileContents);
+
+	    // create js directory
+	    if(jsFiles.length > 0) {
+	    	gutil.log('Processing JS files');
+	    	generateClientLibrarySection('js', clientlibRootDir, jsFiles);
+	    }
+
+	    // create css directory
+	    if(cssFiles.length > 0) {
+	    	gutil.log('Processing CSS files');
+	    	generateClientLibrarySection('css', clientlibRootDir, cssFiles);
+	    }
+
+		//------------------------------
 		if (fileUploaded) {
 			gutil.log(PLUGIN_NAME, gutil.colors.green('CRX Package uploaded successfully'));
 		} else {
@@ -99,10 +159,35 @@ module.exports = function (options) {
 		callback();
 	}
 
+	/*****************************
+     *    UTILITY FUNCTIONS      *
+     *****************************/
+
+    /**
+     * Generates a section of the client library (JS or CSS), creating a js.txt or css.txt.
+     * @param name i.e. 'js' or 'css'
+     * @param clientlibRootDir
+     * @param fileList
+     */
+     function generateClientLibrarySection(name, clientlibRootDir, fileList) {
+     	var destFilePaths = [];
+    	var sectionDir = fs.mkdirsSync(path.join(clientlibRootDir, '/', name));
+
+		fileList.forEach(function(file) {
+			var relativePath = file.relative;
+			var dest = relativePath.substring(relativePath.indexOf(path.sep)+1, relativePath.length);
+			fs.copySync(file.path, path.join(sectionDir, dest));
+			destFilePaths.push(dest);
+		});
+
+		// create .txt file
+	    fs.outputFileSync(path.join(sectionDir, name + '.txt'),
+                  "#base=".concat(name).concat('\n')
+                  .concat(destFilePaths.join('\n')));
+     }
+
 	function isFileInPath(file, path) {
-		gutil.log(file.contents);
-		gutil.log('Checking ', file.relative, ' is in ', path);
-		return fs.realpathSync(file.relative).indexOf(path) == 0;
+		return fs.realpathSync(file.path).indexOf(path) == 0;
 	}
 
 	return through.obj(bufferContents, endStream);
--- test.js
+++ test.js
@@ -7,7 +7,8 @@ var gutil        = require('gulp-util');
 var pathExists   = require('path-exists');
 var gulp         = require('gulp');
 var path         = require('path');
-var should 		 = require('should');
+var should 		   = require('should');
+var debug        = require('gulp-debug');
 
 describe('gulp-clientlibify', function() {
 	before(function () {
@@ -18,50 +19,50 @@ describe('gulp-clientlibify', function() {
 		gutil.log('gulp-clientlibify:', 'finished running test');
 	});
 
-  	it('should throw error when `cssDir` and `jsDir` options are missing', function () {
-        (function() {
-          clientlibify({ 
-          	cssDir: undefined,
-          	jsDir: undefined
-          });
-        }).should.throw('`cssDir` and/or `jsDir` must be provided');
+	it('should throw error when `cssDir` and `jsDir` options are missing', function () {
+      (function() {
+        clientlibify({ 
+        	cssDir: undefined,
+        	jsDir: undefined
+        });
+      }).should.throw('`cssDir` and/or `jsDir` must be provided');
 	});
 
-  	it('should throw error when `cssDir` is not a directory', function () {
-        (function() {
-          clientlibify({ 
-          	cssDir: 'fixtures/css/test.css'
-          });
-        }).should.throw('fixtures/css/test.css is not a directory');
+	it('should throw error when `cssDir` is not a directory', function () {
+      (function() {
+        clientlibify({ 
+        	cssDir: 'fixtures/styles/test.css'
+        });
+      }).should.throw('fixtures/styles/test.css is not a directory');
 	});
 
-  	it('should throw error when `jsDir` is not a directory', function () {
-        (function() {
-          clientlibify({ 
-          	cssDir: 'fixtures/js/test.js'
-          });
-        }).should.throw('fixtures/js/test.js is not a directory');
+	it('should throw error when `jsDir` is not a directory', function () {
+      (function() {
+        clientlibify({ 
+        	cssDir: 'fixtures/scripts/test.js'
+        });
+      }).should.throw('fixtures/scripts/test.js is not a directory');
 	});
 
 	it('should emit error on streamed file', function (done) {
 		gulp.src('fixtures/**/*', { buffer: false })
 		  .pipe(clientlibify({
-		  	cssDir: 'fixtures/css',
-		  	jsDir: 'fixtures/js'
+		  	cssDir: 'fixtures/styles',
+		  	jsDir: 'fixtures/scripts'
 		  }))
-    	  .on('error', function (err) {
-      		err.message.should.eql('Streaming not supported');
-      		done();
+  	  .on('error', function (err) {
+    		err.message.should.eql('Streaming not supported');
+    		done();
 		  });
 	});
 
-    it('should process all files', function (done) {
+  it('should process all files', function (done) {
       gulp.src('fixtures/**/*')
         .pipe(clientlibify({
-		  	cssDir: 'fixtures/css',
-		  	jsDir: 'fixtures/js'
-		}))
-        .pipe(assert.length(15))
+		  	   cssDir: 'fixtures/styles',
+		  	   jsDir: 'fixtures/scripts'
+		    }))
+        .pipe(assert.length(17))
         .pipe(assert.end(done));
 	});
 });
